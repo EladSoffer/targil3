5c557e804216f46ea66a8b8ebaefc8bf2aef07c9
EladSoffer <118102820+EladSoffer@users.noreply.github.com>
2023-06-01 10:08:53 +0300

    change

--- src/index.js
+++ src/index.js
@@ -1,33 +1,69 @@
-import React, { useState } from 'react';
-import ReactDOM from 'react-dom/client';
+import React, { useState, useEffect } from 'react';
+import ReactDOM from 'react-dom';
 import './index.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import io from 'socket.io-client';
 
 import Login from './login/Login';
 import Chat from './chat/Chat';
 import Register from './register/Register';
 
-
+const socket = io('http://localhost:5000'); // Replace with your server URL
+socket.emit ('foo', 'bar');
 
 const App = () => {
-  const [user, setUser] = useState([]); // Initialize user state as an empty array
-  const [curuser, setcuruser] = useState({}); // initial value is an empty object
-  const [token,settoken] = useState();
+  const [user, setUser] = useState([]);
+  const [curuser, setcuruser] = useState({});
+  const [token, settoken] = useState();
   const [init, setinit] = useState(0);
 
+  useEffect(() => {
+    console.log("dd");
+    // Connect to the socket when the component mounts
+    socket.connect();
+
+    // Clean up the socket connection when the component unmounts
+    return () => {
+      socket.disconnect();
+    };
+  }, []);
 
   return (
     <BrowserRouter>
       <Routes>
-
-        <Route path="/" element={<Login setcuruser={setcuruser} user={user} setinit={setinit} settoken={settoken}/>} />
-        <Route path="/Register" element={<Register setUser={setUser} user={user} init={init}/>} />
-        <Route path="/Chat" element={<Chat curuser ={curuser} setcuruser={setcuruser} user={user} init={init} token={token}/>} />
-
+        <Route
+          path="/"
+          element={
+            <Login
+              setcuruser={setcuruser}
+              user={user}
+              setinit={setinit}
+              settoken={settoken}
+              socket={socket}
+            />
+          }
+        />
+        <Route
+          path="/Register"
+          element={<Register setUser={setUser} user={user} init={init} />}
+        />
+        <Route
+          path="/Chat"
+          element={
+            <Chat
+              curuser={curuser}
+              setcuruser={setcuruser}
+              user={user}
+              init={init}
+              token={token}
+              socket={socket}
+            />
+          }
+        />
       </Routes>
     </BrowserRouter>
   );
 };
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+const root = document.getElementById('root');
+ReactDOM.render(<App />, root);
